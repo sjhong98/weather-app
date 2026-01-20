a72c082c94896de3e5fb4556b7f06943bb0c08d9
seungjaehongibrdos <162073073+seungjaehongibrdos@users.noreply.github.com>
2026-01-20 16:37:12 +0900

    fix: build error

--- src/entities/location-bookmark/model/useBookmark.tsx
+++ src/entities/location-bookmark/model/useBookmark.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect } from "react"
 
 import { getBookmarkList, useBookmarkContext } from "@/entities/location-bookmark"
 
